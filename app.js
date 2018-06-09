5d0052b15abd52d3950163a803c65fbfda987160
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-09 21:17:35 +0900

    a

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ var app = new Vue({
   el: '#app',
   data: {
     scanner: null,
-    activeCameraId: null,
-    cameras: [],
-    scans: []
+    activeCameraIndex: null,
+    cameras: []
   },
   mounted: function () {
     var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+    self.scanner = new Instascan.Scanner({
+      video: document.getElementById('preview'),
+      scanPeriod: 5,
+      mirror: false
+    });
     self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content });
+      console.log(content);
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
-      console.log(cameras);
-      if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
+      if (self.cameras.length > 0) {
+        self.activeCameraIndex = self.cameras.length - 1;
+        self.scanner.start(self.cameras[self.activeCameraIndex]);
       } else {
         console.error('No cameras found.');
       }
@@ -26,12 +28,12 @@ var app = new Vue({
     });
   },
   methods: {
-    formatName: function (name) {
-      return name || '(unknown)';
-    },
-    selectCamera: function (camera) {
-      this.activeCameraId = camera.id;
-      this.scanner.start(camera);
+    changeCamera: function () {
+      this.activeCameraIndex = this.activeCameraIndex + 1;
+      if (this.activeCameraIndex === this.cameras.length) {
+        this.activeCameraIndex = 0;
+      }
+      this.scanner.start(this.cameras[this.activeCameraIndex]);
     }
   }
 });
